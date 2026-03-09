7272a07772e50e9b09b0d064dddc73ad854cfaa7
خالد رافغ <khaalidrafi@gmail.com>
2026-03-09 11:45:17 +0600

    Handle login with JS

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+const loginBtn = document.querySelector('#login-btn');
+const username = document.querySelector("#username");
+const password = document.querySelector('#password');
+
+loginBtn.addEventListener("click", () => {
+    if (username.value === "admin" && password.value === "admin123") {
+        window.location.assign("/issues.html");
+    } else {
+        alert("Invalid username or password! Please use default credentials.")
+    }
+})
+
