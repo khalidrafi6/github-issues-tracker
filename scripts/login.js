ed30b610bbf6406dd43333384c0dca75d6db7b3a
خالد رافغ <khaalidrafi@gmail.com>
2026-03-10 11:43:58 +0600

    Use <form> for login

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,8 @@ const loginBtn = document.querySelector("#login-btn");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 
-loginBtn.addEventListener("click", () => {
+loginBtn.addEventListener("click", (event) => {
+    event.preventDefault();
   if (username.value === "admin" && password.value === "admin123") {
     window.location.assign("./issues.html");
   } else {
