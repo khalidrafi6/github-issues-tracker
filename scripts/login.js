614289e644805247754ffb7d7db7166e876436d2
خالد رافغ <khaalidrafi@gmail.com>
2026-03-12 16:11:24 +0600

    Fix login

--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,18 @@
 const loginForm = document.querySelector("#login");
+const demoData = new FormData();
+
+demoData.append("username", "admin");
+demoData.append("password", "admin123");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const loginData = new FormData(loginForm);
 
-  const demoData = new FormData();
-
-  demoData.append("username", "admin");
-  demoData.append("password", "admin123");
-
-  if (JSON.stringify(loginData) === JSON.stringify(demoData)) {
+  if (
+    JSON.stringify([...loginData.entries()]) ===
+    JSON.stringify([...demoData.entries()])
+  ) {
     window.location.assign("./issues.html");
   } else {
     alert("Invalid username or password! Please use demo credentials.");
